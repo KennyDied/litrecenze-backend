17bf1d12ff3d1d99a550512923ccd0726607a7f3
Danila Bykovsky <thekennydied@mail.ru>
2022-04-16 17:08:28 +0300

    Added swagger

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,19 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { AppModule } from './modules/app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 (async () => {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('LitRecenze API')
+    .setDescription('API description')
+    .setVersion('1.0')
+    .addTag('Public')
+    .addTag('Guarded')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 })();
